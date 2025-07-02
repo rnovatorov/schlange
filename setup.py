341f754c74f95471fd84ed9d8d36ddc1ac4328be
Roman Novatorov <roman.novatorov@gmail.com>
2025-07-02 13:23:08 +0300

    rename to flockq

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="flockueue",
+    name="flockq",
     version="0.0.1",
     description="Persistent task queue built with flock",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
-    url="https://github.com/rnovatorov/python-flockueue",
+    url="https://github.com/rnovatorov/python-flockq",
     author="Roman Novatorov",
     author_email="roman.novatorov@gmail.com",
     install_requires=[],
