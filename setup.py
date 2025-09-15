bf9bf94d6a858f2dca84bae5e6ad134e8aefe457
Roman Novatorov <roman.novatorov@gmail.com>
2025-09-15 22:47:09 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="schlange-queue",
-    version="0.1.0",
+    version="0.1.1",
     description="Lightweight, persistent, single-node task queue with at-least-once delivery guarantee",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
